54fa88b3158bf42d5ff0455b5fd35b5c3fa2fbaf
samettas52 <166943077+samettas52@users.noreply.github.com>
2024-10-12 00:09:28 +0300

    c-sharp-ders3

---
+++ ders3/ders3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ders3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //iki tane int değer belirle ve ternary operatörü ile büyük olanı bul
+            int deger1 = 123;
+            int deger2 = 124;
+            bool sonuc = deger2 > deger1;
+            if (sonuc== true )
+            {
+                Console.WriteLine("Büyük olan değer : " + deger2);
+            }
+            else
+            {
+                Console.WriteLine("Büyük olan değer : " + deger1);
+            }
+            Console.Read();
+        }
+    }
+}
